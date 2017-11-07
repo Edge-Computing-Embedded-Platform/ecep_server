e53e15724f4f99f8af058818c1692b5868f9618a
Abhishek Gurudurtt <abhishekgurudurtt@Abhisheks-MacBook-Pro.local>
2017-11-06 21:31:26 -0500

    added nodejs installation command

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,3 +4,10 @@
 /usr/local/bin/python2.7 -m pip install crossbar autobahn
 /usr/local/bin/python2.7 -m pip install tornado
 /usr/local/bin/python2.7 -m pip install futures
+
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+    pcurl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+fi
